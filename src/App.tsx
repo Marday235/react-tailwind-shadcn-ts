e4206b402364721e9a6894a27b787a0c8f28841f
Abdoulaye Wardougou Khamis <mardaywardougou9@gmail.com>
2025-11-07 13:50:42 +0100

    up

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click m?</Button>
+      <Button>Click me</Button>
     </div>
   )
 }
